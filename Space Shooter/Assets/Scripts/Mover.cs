4a5cc9149e9a7e315632b1606193eb81b7488adc
DandeOne <dande2@o2.pl>
2017-02-14 21:43:10 +0100

    Shooting system for player and Bolt prefab

---
+++ Space Shooter/Assets/Scripts/Mover.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+    private Rigidbody rb;
+    public float speed;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        speed = 20;
+    }
+	// Use this for initialization
+	void Start () {
+        rb.velocity = transform.forward * speed;
+        Destroy(gameObject, 2.2f);
+	}
+    void Update()
+    {
+
+    }
+	
+	
+}
---
+++ Space Shooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float tilt;
+    public Boundary boundary;
+    private Rigidbody rb;
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        speed = 10;
+        boundary.xMin = -10;
+        boundary.xMax = 10;
+        boundary.zMin = -4;
+        boundary.zMax = 5;
+        tilt = 4;
+    }
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+
+    private float nextFire;
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+        }
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.velocity = movement * speed;
+
+        rb.position = new Vector3
+        (
+            Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+        );
+
+        rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt);
+    }
+}
